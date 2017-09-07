302fb8ad401dff2aff9bb4e0ae9bda35e81551ba
Sean Henry <hello@seanhenry.codes>
2017-09-07 13:42:17 -0700

    Adds remaining stub return test cases

--- TestProject/ReturnProtocol.swift
+++ TestProject/ReturnProtocol.swift
@@ -1,13 +1,13 @@
 protocol ReturnProtocol {
     func returnType() -> String
-//    func returnTuple() -> (String, Int?)
-//    func returnLabelledTuple() -> (s: String, i: Int?)
-//    func returnOptional() -> Int?
-//    func returnIUO() -> UInt!
-//    func returnGeneric() -> Optional<String>
-//    func returnOptionalGeneric() -> Optional<String>?
-//    func returnClosure() -> () -> ()
-//    func returnComplicatedClosure() -> ((String, Int?) -> (UInt))
-//    func returnOptionalClosure() -> (() -> ())?
-//    func returnExplicitVoid() -> Void
+    func returnTuple() -> (String, Int?)
+    func returnLabelledTuple() -> (s: String, i: Int?)
+    func returnOptional() -> Int?
+    func returnIUO() -> UInt!
+    func returnGeneric() -> Optional<String>
+    func returnOptionalGeneric() -> Optional<String>?
+    func returnClosure() -> () -> ()
+    func returnComplicatedClosure() -> ((String, Int?) -> (UInt))
+    func returnOptionalClosure() -> (() -> ())?
+    func returnExplicitVoid() -> Void
 }
