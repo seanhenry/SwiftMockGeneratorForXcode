41a142de44affa75f40e9802252cdb70fb4c8d98
Sean Henry <hello@seanhenry.codes>
2018-02-24 11:56:31 +1100

    Simplifies XPC connection and extension code

--- XcodePlugin/SourceEditorCommand.swift
+++ XcodePlugin/SourceEditorCommand.swift
@@ -2,28 +2,16 @@ import Foundation
 import XcodeKit
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
-    
-    private var cancelled = false
-    fileprivate lazy var connection: Connection = {
+
+    fileprivate var connection: Connection = {
         let connection = Connection()
-        connection.setUpConnection()
+        connection.connection = SourceEditorExtension.connection!
         return connection
     }()
-    
+
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) -> Void {
-        invocation.cancellationHandler = { [weak self] in
-            DispatchQueue.main.async { [weak self] in
-                self?.cancelled = true
-                self?.connection.suspendConnection()
-            }
-        }
-        connection.interruptionHandler { [weak self] in
-            self?.finish(with: self?.createError("The XPC service was interrupted"), handler: completionHandler)
-        }
-        connection.invalidationHandler { [weak self] in
-            self?.connection.shutdownConnection()
-            self?.finish(with: self?.createError("The XPC service was invalidated"), handler: completionHandler)
-        }
+        setCancelHandler(invocation, completionHandler)
+        setErrorHandlers(completionHandler)
         connection.resumeConnection()
         connection.remoteObjectProxyWithErrorHandler({ [weak self] proxy in
             self?.generateMock(proxy: proxy, invocation: invocation, completionHandler: completionHandler)
@@ -31,39 +19,63 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
             self?.finish(with: error, handler: completionHandler)
         }
     }
-    
-    private func generateMock(proxy: MockGeneratorXPCProtocol, invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) {
-        let selections = invocation.buffer.selections
-        guard selections.count > 0 else {
-            finish(with: createError("No selections. Place the cursor within the class declaration"), handler: completionHandler)
-            return
-        }
-        guard let range = selections.object(at: 0) as? XCSourceTextRange else {
-            finish(with: createError("First selection was not a text range"), handler: completionHandler)
-            return
-        }
-        let actualLineNumber = range.start.line + 1
-        proxy.generateMock(fromFileContents: invocation.buffer.completeBuffer, line: actualLineNumber, column: range.start.column) { [weak self] (result, error) in
-            DispatchQueue.main.async {
-                self?.handleGenerateMock(invocation: invocation, result: result, error: error, completionHandler: completionHandler)
-            }
+
+    private func setCancelHandler(_ invocation: XCSourceEditorCommandInvocation, _ completionHandler: @escaping (Error?) -> Void) {
+        invocation.cancellationHandler = { [weak self] in
+            self?.finish(with: self?.createError("The operation was cancelled"), handler: completionHandler)
         }
     }
 
-    private func handleGenerateMock(invocation: XCSourceEditorCommandInvocation, result: [String]?, error: Error?, completionHandler: @escaping (Error?) -> Void) {
-        if cancelled {
-            finish(with: createError("The operation was cancelled"), handler: completionHandler)
-            return
+    private func setErrorHandlers(_ completionHandler: @escaping (Error?) -> Void) {
+        connection.interruptionHandler { [weak self] in
+            self?.finish(with: self?.createError("The XPC service was interrupted"), handler: completionHandler)
         }
+        connection.invalidationHandler { [weak self] in
+            self?.finish(with: self?.createError("The XPC service was invalidated"), handler: completionHandler)
+        }
+    }
+
+    private func finish(with error: Error?, handler: (Error?) -> ()) {
+        connection.suspendConnection()
+        handler(error)
+    }
+    
+    private func generateMock(proxy: MockGeneratorXPCProtocol, invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) {
+        do {
+            try tryToGenerateMock(proxy: proxy, invocation: invocation) { [weak self] (result, error) in
+                self?.handleGenerateMock(invocation: invocation, result: result, error: error, completionHandler: completionHandler)
+            }
+        } catch {
+            finish(with: error, handler: completionHandler)
+        }
+    }
+
+    private func tryToGenerateMock(proxy: MockGeneratorXPCProtocol, invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping ([String]?, Error?) -> Void) throws {
+        let range = try getRange(selections: try getSelection(invocation: invocation))
+        let actualLineNumber = range.start.line + 1
+        proxy.generateMock(fromFileContents: invocation.buffer.completeBuffer, line: actualLineNumber, column: range.start.column, withReply: completionHandler)
+    }
+
+    private func getSelection(invocation: XCSourceEditorCommandInvocation) throws -> NSMutableArray {
+        let selections = invocation.buffer.selections
+        guard selections.count > 0 else {
+            throw createError("No selections. Place the cursor within the class declaration")
+        }
+        return selections
+    }
+
+    private func getRange(selections: NSMutableArray) throws -> XCSourceTextRange {
+        guard let range = selections.object(at: 0) as? XCSourceTextRange else {
+            throw createError("First selection was not a text range")
+        }
+        return range
+    }
+
+    private func handleGenerateMock(invocation: XCSourceEditorCommandInvocation, result: [String]?, error: Error?, completionHandler: @escaping (Error?) -> Void) {
         if let result = result {
             invocation.buffer.completeBuffer = result.joined(separator: "\n")
         }
         finish(with: error, handler: completionHandler)
-    }
-    
-    private func finish(with error: Error?, handler: (Error?) -> ()) {
-        connection.suspendConnection()
-        handler(error)
     }
     
     private func createError(_ message: String) -> Error {
@@ -72,90 +84,33 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     fileprivate class Connection {
         
-        static var connection: NSXPCConnection?
-        static var isRunning = false
-        private static let connectionQueue = DispatchQueue(label: "codes.seanhenry.mockgenerator.connectionQueue")
-        
-        func setUpConnection() {
-            synchronise { _ in
-                if Connection.connection == nil {
-                    Connection.connection = NSXPCConnection(serviceName: "codes.seanhenry.MockGeneratorXPC")
-                    Connection.connection?.remoteObjectInterface = NSXPCInterface(with: MockGeneratorXPCProtocol.self)
-                    Connection.isRunning = false
-                }
-            }
-        }
-        
+        var connection: NSXPCConnection?
+
         func resumeConnection() {
-            synchronise { connection in
-                if !Connection.isRunning {
-                    connection?.resume()
-                    Connection.isRunning = true
-                }
-            }
+            connection?.resume()
         }
-        
+
         func suspendConnection() {
-            synchronise { connection in
-                if Connection.isRunning {
-                    connection?.suspend()
-                    Connection.isRunning = false
-                }
-            }
-        }
-        
-        func shutdownConnection() {
-            synchronise { connection in
-                connection?.suspend()
-                connection?.invalidate()
-                Connection.connection = nil
-            }
+            connection?.suspend()
         }
         
         func interruptionHandler(_ closure: @escaping () -> ()) {
-            synchronise { connection in
-                connection?.interruptionHandler = {
-                    self.mainThread { closure() }
-                }
-            }
+            connection?.interruptionHandler = closure
         }
-        
+
         func invalidationHandler(_ closure: @escaping () -> ()) {
-            synchronise { connection in
-                connection?.invalidationHandler = {
-                    self.mainThread { closure() }
-                }
-            }
+            connection?.invalidationHandler = closure
+            SourceEditorExtension.setUpConnection()
         }
         
         func remoteObjectProxyWithErrorHandler(_ closure: @escaping (MockGeneratorXPCProtocol) -> (), _ errorHandler: @escaping (Error) -> ()) {
-            synchronise { [weak self] connection in
-                let proxy = connection?.remoteObjectProxyWithErrorHandler { [weak self] error in
-                    self?.mainThread { errorHandler(error) }
-                }
-                if let mockGenerator = proxy as? MockGeneratorXPCProtocol {
-                    self?.mainThread {
-                        closure(mockGenerator)
-                    }
-                } else {
-                    fatalError("XPCConnection is set up with the wrong protocol type")
-                }
+            let proxy = connection?.remoteObjectProxyWithErrorHandler { error in
+                errorHandler(error)
             }
-        }
-        
-        func synchronise(_ closure: @escaping (NSXPCConnection?) -> ()) {
-            Connection.connectionQueue.async {
-                closure(Connection.connection)
-            }
-        }
-        
-        func mainThread(_ closure: @escaping () -> ()) {
-            if Thread.isMainThread {
-                closure()
-                return
-            }
-            DispatchQueue.main.async {
-                closure()
+            if let generator = proxy as? MockGeneratorXPCProtocol {
+                closure(generator)
+            } else {
+                errorHandler(NSError(domain: "codes.seanhenry.mockgenerator", code: 0, userInfo: [NSLocalizedDescriptionKey: "The XPC service failed to return a proxy."]))
             }
         }
     }
--- XcodePlugin/SourceEditorExtension.swift
+++ XcodePlugin/SourceEditorExtension.swift
@@ -4,4 +4,14 @@ import Cocoa
 
 class SourceEditorExtension: NSObject, XCSourceEditorExtension {
 
+    static var connection: NSXPCConnection!
+
+    static func setUpConnection() {
+        connection = NSXPCConnection(serviceName: "codes.seanhenry.MockGeneratorXPC")
+        connection?.remoteObjectInterface = NSXPCInterface(with: MockGeneratorXPCProtocol.self)
+    }
+
+    func extensionDidFinishLaunching() {
+        SourceEditorExtension.setUpConnection()
+    }
 }
