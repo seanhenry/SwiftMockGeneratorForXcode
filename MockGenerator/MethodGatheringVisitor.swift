60e12d05354d8a57f2d9a7011c9e87ece7be8919
Sean Henry <hello@seanhenry.codes>
2018-03-10 18:31:59 +1100

    Simplifies MethodGatheringVisitor using new structure

--- MockGenerator/MethodGatheringVisitor.swift
+++ MockGenerator/MethodGatheringVisitor.swift
@@ -23,65 +23,34 @@ class MethodGatheringVisitor: ElementVisitor {
         return UseCasesProtocolMethod(
             name: method.name,
             returnType: method.returnType?.text,
-            resolvedReturnType: resolveReturnType(method.returnType),
+            resolvedReturnType: resolveType(method.returnType),
             parametersList: transformParameters(from: method),
             signature: method.text,
             throws: false
         )
     }
 
-    private func resolveReturnType(_ type: Element?) -> UseCasesType? {
-        guard let type = type else { return nil }
-        let resolved = ResolveUtil().resolveToElement(type)
-        if let generic = resolved as? GenericParameterTypeDeclaration {
-            return UseCasesGenericType(typeName: generic.text)
-        }
-        return UseCasesType(typeName: type.text)
-    }
-
     private func transformParameters(from method: SwiftMethodElement) -> [UseCasesParameter] {
-        let parameters = parseParameters(from: method)
-        let resolvedTypes = getResolvedParameterTypes(from: method)
-        return transformParameters(parameters, resolvedTypes: resolvedTypes)
+        return method.parameters.map { p in
+            UseCasesParameter(
+                label: p.externalParameterName ?? p.localParameterName,
+                name: p.localParameterName,
+                type: p.type.text,
+                resolvedType: resolveType(p.type) ?? UseCasesType(typeName: p.type.text),
+                text_: p.text)
+        }
     }
 
-    private func parseParameters(from method: SwiftMethodElement) -> [UseCasesParameter] {
-        return UseCasesProtocolMethod( // Kotlin native doesn't give access to ParameterUtil
-            name: "",
-            returnType: nil,
-            parameters: getParametersText(element:  method),
-            signature: ""
-        ).parametersList as! [UseCasesParameter]
-    }
-
-    private func getParametersText(element: SwiftMethodElement) -> String {
-        return element.parameters.map { $0.text }.joined(separator: ", ")
-    }
-
-    private func getResolvedParameterTypes(from element: SwiftMethodElement) -> [UseCasesType?] {
-        return element.parameters.map(transformToParameterType)
-    }
-
-    private func transformToParameterType(_ parameter: MethodParameter) -> UseCasesType? {
+    private func resolveType(_ type: Element?) -> UseCasesType? {
+        guard let type = type else { return nil }
         var result: UseCasesType?
-        let resolved = ResolveUtil().resolveToElement(parameter.type)
+        let resolved = ResolveUtil().resolveToElement(type)
         if let alias = resolved as? Typealias {
             result = UseCasesType(typeName: alias.typeName)
         } else if let genericType = resolved as? GenericParameterTypeDeclaration {
             result = UseCasesGenericType(typeName: genericType.text)
         }
         return result
-    }
-
-    private func transformParameters(_ parameters: [UseCasesParameter], resolvedTypes: [UseCasesType?]) -> [UseCasesParameter] {
-        let parametersAndTypes = zip(parameters, resolvedTypes)
-        return parametersAndTypes.map { param, resolved in
-            UseCasesParameter(label: param.label,
-                name: param.name,
-                type: param.type,
-                resolvedType: resolved ?? UseCasesType(typeName: param.type),
-                text_: param.text)
-        }
     }
 
     func visit(_ element: SwiftPropertyElement) {
--- SwiftStructureInterface/structure/model/MethodParameter.swift
+++ SwiftStructureInterface/structure/model/MethodParameter.swift
@@ -1,3 +1,5 @@
 protocol MethodParameter: Element {
     var type: Element { get }
+    var externalParameterName: String? { get }
+    var localParameterName: String { get }
 }
