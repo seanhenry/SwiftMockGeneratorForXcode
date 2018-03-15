28406af8527d9e2758f18e30228637e5913fbd9e
Sean Henry <hello@seanhenry.codes>
2018-03-15 11:25:33 +1100

    Adds loc to analytics

--- MockGeneratorXPC/GoogleAnalyticsTracker.swift
+++ MockGeneratorXPC/GoogleAnalyticsTracker.swift
@@ -5,10 +5,10 @@ class GoogleAnalyticsTracker {
     private let tid = "UA-114749580-3"
 //    private let tid = "UA-114749580-1" // Debug
 
-    func track(action: String) {
+    func track(action: String, value: Int = 0) {
         var request = URLRequest(url: URL(string: "https://www.google-analytics.com/collect")!)
         request.httpMethod = "POST"
-        request.httpBody = "v=1&tid=\(tid)&cid=Xcode&t=event&ec=mock&ea=\(action)"
+        request.httpBody = "v=1&tid=\(tid)&cid=Xcode&t=event&ec=mock&ea=\(action)&ev=\(value)"
             .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)?
             .data(using: .utf8)
         URLSession.shared.dataTask(with: request).resume()
--- MockGeneratorXPC/MockGeneratorXPC.swift
+++ MockGeneratorXPC/MockGeneratorXPC.swift
@@ -6,29 +6,31 @@ import Foundation
     private static var tracker = GoogleAnalyticsTracker()
 
     func generateMock(fromFileContents contents: String, line: Int, column: Int, withReply reply: @escaping ([String]?, Error?) -> Void) {
+        let completion: ([String]?, Error?) -> () = { lines, error in
+            self.track(lines, error)
+            reply(lines, error)
+        }
         let (projectURL, error) = ProjectFinder(projectFinder: XcodeProjectPathFinder(), preferences: Preferences()).getProjectPath()
         if let projectURL = projectURL {
-            generateMock(projectURL: projectURL, contents: contents, line: line, column: column, reply: reply)
+            generateMock(projectURL: projectURL, contents: contents, line: line, column: column, completion: completion)
         } else {
-            track(error)
-            reply(nil, error)
+            completion(nil, error)
         }
     }
 
-    private func generateMock(projectURL: URL, contents: String, line: Int, column: Int, reply: ([String]?, Error?) -> Void) {
+    private func generateMock(projectURL: URL, contents: String, line: Int, column: Int, completion: ([String]?, Error?) -> Void) {
         let (lines, error) = Generator.generateMock(fromFileContents: contents,
             projectURL: projectURL,
             line: line,
             column: column)
-        track(error)
-        reply(lines, error)
+        completion(lines, error)
     }
 
-    private func track(_ error: Error?) {
+    private func track(_ lines: [String]?, _ error: Error?) {
         if let error = error {
             MockGeneratorXPC.tracker.track(action: error.localizedDescription)
-        } else {
-            MockGeneratorXPC.tracker.track(action: "generated")
+        } else if let lines = lines {
+            MockGeneratorXPC.tracker.track(action: "generated", value: lines.count)
         }
     }
 }
