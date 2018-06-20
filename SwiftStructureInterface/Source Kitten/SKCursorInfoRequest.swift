022e27351b3e94fcc774dea5bee8defa907f81e7
Sean <seanhenry@nbnco.com.au>
2018-06-20 11:39:51 +1000

    Improves performance by processing swift files in parallel

--- SwiftStructureInterface/Source Kitten/SKCursorInfoRequest.swift
+++ SwiftStructureInterface/Source Kitten/SKCursorInfoRequest.swift
@@ -3,13 +3,19 @@ import SourceKittenFramework
 public class SKCursorInfoRequest: CursorInfoRequest {
 
     private let files: [String]
+    private static let cores: Int = {
+        return ProcessInfo.processInfo.processorCount
+    }()
+    private static let parallelOption: String = {
+        return "-j\(cores)"
+    }()
 
     public init(files: [String]) {
         self.files = files
     }
 
     public func getCursorInfo(filePath: String, offset: Int64) throws -> [String: Any] {
-        let arguments = files + [filePath]
+        let arguments = [SKCursorInfoRequest.parallelOption] + files + [filePath]
         return try Request.cursorInfo(file: filePath, offset: offset, arguments: arguments).send()
     }
 }
