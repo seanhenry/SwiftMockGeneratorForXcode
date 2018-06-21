0c8d5baa0db0651a7cbceedd3fdb7c88c1ddf3a2
Sean Henry <hello@seanhenry.codes>
2018-06-21 21:07:46 +1000

    Adds method for formatting lines relative to an element

--- SwiftStructureInterface/structure/util/FormatUtil.swift
+++ SwiftStructureInterface/structure/util/FormatUtil.swift
@@ -3,8 +3,17 @@ public class FormatUtil {
     public static var formatRequest: FormatRequest!
     private let tempFileWriter = TempFileWriterUtil()
     private var tempFile: String { return tempFileWriter.tempFile }
+    private let useTabs: Bool
+    private let spaces: Int
 
-    public init() {}
+    public convenience init() {
+        self.init(useTabs: false, spaces: 4)
+    }
+
+    public init(useTabs: Bool, spaces: Int) {
+        self.useTabs = useTabs
+        self.spaces = spaces
+    }
 
     public func format(_ element: Element) -> Element {
         guard let file = element.file else { return element }
@@ -17,5 +26,18 @@ public class FormatUtil {
 
     private func format() -> String? {
         return try? FormatUtil.formatRequest.format(filePath: tempFile)
+    }
+
+    public func format(_ lines: [String], in element: Element) -> [String] {
+        guard let file = element.file,
+              let (_, column) = LocationConverter.convert(caretOffset: element.offset, in: file.text) else {
+            return lines
+        }
+        let start = file.text.index(file.text.startIndex, offsetBy: Int(element.offset) - column)
+        let end = file.text.index(start, offsetBy: column)
+        let elementIndent = String(file.text[start..<end])
+        let formatted = NaiveFormatter(useTabs: useTabs, spaces: spaces).format(lines: lines)
+        let indent = useTabs ? "\t" : String(repeating: " ", count: spaces)
+        return formatted.map { "\(elementIndent)\(indent)\($0)" }
     }
 }
--- SwiftStructureInterfaceTests/structure/util/FormatUtilTests.swift
+++ SwiftStructureInterfaceTests/structure/util/FormatUtilTests.swift
@@ -8,7 +8,7 @@ class FormatUtilTests: XCTestCase {
     override func setUp() {
         super.setUp()
         FormatUtil.formatRequest = SKFormatRequest()
-        util = FormatUtil()
+        util = FormatUtil(useTabs: false, spaces: 2)
     }
 
     override func tearDown() {
@@ -46,6 +46,78 @@ class FormatUtilTests: XCTestCase {
         let inFile = SKElementFactoryTestHelper.build(from: getExampleString())!
         let outFile = util.format(inFile)
         XCTAssert(outFile === inFile)
+    }
+
+    // MARK: - format(in:)
+
+    func test_format_findAdd1LevelOfIndentWhenParentHasNoIndent() {
+        let file = """
+        class A {
+        }
+        """
+        assertFormat("var a: A", "  var a: A", in: file)
+    }
+
+    func test_format_findIndentFromElementAndAddItToFormattedLines() {
+        let file = """
+             class A {
+        }
+        """
+        assertFormat("var a: A", "       var a: A", in: file)
+    }
+
+    func test_format_shouldFormatLinesRelativeToParent() {
+        let file = """
+          class A {
+          }
+        """
+        let input = """
+        func a() {
+        invokedA = true
+        }
+        """
+        let expected = """
+            func a() {
+              invokedA = true
+            }
+        """
+        assertFormat(input, expected, in: file)
+    }
+
+    func test_format_shouldIndentUsingTabs() {
+        util = FormatUtil(useTabs: true, spaces: 2)
+        let file = """
+        \tclass A {
+        }
+        """
+        assertFormat("var a: A", "\t\tvar a: A", in: file)
+    }
+
+    func test_format_shouldIndentUsingDifferentNumberOfSpaces() {
+        util = FormatUtil(useTabs: false, spaces: 4)
+        let file = """
+            class A {
+        }
+        """
+        assertFormat("var a: A", "        var a: A", in: file)
+    }
+
+    func test_format_shouldCopyElementIndentAndAddPreferredIndentOnTop() {
+        util = FormatUtil(useTabs: false, spaces: 4)
+        let file = """
+        \t  \tclass A {
+        }
+        """
+        assertFormat("var a: A", "\t  \t    var a: A", in: file)
+    }
+
+    private func assertFormat(_ input: String, _ expected: String, in file: String, line: UInt = #line) {
+        let inputLines = input.components(separatedBy: "\n")
+        let expectedLines = expected.components(separatedBy: "\n")
+        let element = SKElementFactoryTestHelper.build(from: file)!.typeDeclarations[0]
+        let formatted = util.format(inputLines, in: element)
+        XCTAssertFalse(formatted.isEmpty)
+        zip(formatted, expectedLines).forEach { XCTAssertEqual($0, $1, line: line) }
     }
 
     // MARK: - Helpers
