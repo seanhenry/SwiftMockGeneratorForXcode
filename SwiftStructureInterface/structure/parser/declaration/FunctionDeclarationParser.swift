32eb456741f97b37d1d7878bc7aa77ba1b95fc3e
Sean <seanhenry@nbnco.com.au>
2018-06-27 14:22:48 +1000

    FunctionDeclaration

--- SwiftStructureInterface/structure/parser/declaration/FunctionDeclarationParser.swift
+++ SwiftStructureInterface/structure/parser/declaration/FunctionDeclarationParser.swift
@@ -2,66 +2,25 @@ import Source
 
 class FunctionDeclarationParser: DeclarationParser<FunctionDeclaration> {
 
-    override func parseDeclaration(start: LineColumn, children: [Any?]) -> FunctionDeclaration {
-        var name = ""
-        try! appendIdentifier(to: &name)
-        let genericParameterClause = parseGenericParameterClause()
-        let parameters = parseParameterClause()
-        let `throws` = parseThrows()
-        skipRethrows()
-        let returnType = parseReturnType()
-        skipWhereClause()
-        return createElement(start: start) { offset, length, text in
-            return FunctionDeclarationImpl(
-                text: text,
-                offset: offset,
-                length: length,
-                name: name,
-                genericParameterClause: genericParameterClause,
-                parameters: parameters,
-                returnType: returnType,
-                throws: `throws`,
-                declarations: [])
-        } ?? FunctionDeclarationImpl.emptyFunctionDeclaration
+    override func parseDeclaration(builder: ParserBuilder) throws -> FunctionDeclaration {
+        return try FunctionDeclarationImpl(children:builder
+                .optional { try self.parseIdentifier() }
+                .optional { try self.parseGenericParameterClause() }
+                .optional { try self.parseParameterClause() }
+                .optional { try self.parseThrows() }
+                .optional { try self.parseFunctionDeclarationResult() }
+                .optional { try self.parseWhereClause() }
+                .build())
     }
 
-    private func parseParameterClause() -> [Parameter] {
-        return parseFunctionDeclarationParameterClause()
-    }
+    class ResultParser: Parser<FunctionResult> {
 
-    private func parseThrows() -> Bool {
-        if isNext(.throws) {
-            advance()
-            return true
-        }
-        return false
-    }
-
-    private func skipRethrows() {
-        advance(if: .rethrows)
-    }
-
-    private func parseReturnType() -> Element? {
-        let result = parseFunctionDeclarationResult()
-        if result !== TypeImpl.emptyType {
-            return result
-        }
-        return nil
-    }
-
-    private func skipWhereClause() {
-        _ = parseWhereClause()
-    }
-
-    class ResultParser: Parser<Element> {
-
-        override func parse(start: LineColumn) -> Element {
-            guard isNext(.arrow) else {
-                return TypeImpl.emptyType
-            }
-            advance()
-            _ = parseAttributes()
-            return parseType()
+        override func parse() throws -> FunctionResult {
+            return try FunctionResultImpl(children: builder()
+                    .required { try self.parsePunctuation(.arrow) }
+                    .optional { try self.parseAttributes() }
+                    .optional { try self.parseType() }
+                    .build())
         }
     }
 }
--- SwiftStructureInterfaceTests/structure/parser/declaration/FunctionDeclarationParserTests.swift
+++ SwiftStructureInterfaceTests/structure/parser/declaration/FunctionDeclarationParserTests.swift
@@ -8,88 +8,92 @@ class FunctionDeclarationParserTests: XCTestCase {
 
     // MARK: - parse
 
-    func test_parse_shouldParseFuncWithEmptyParameterClause() {
+    func test_parse_shouldParseFuncWithEmptyParameterClause() throws {
         let text = "func a()"
-        let function = parse(text)
+        let function = try parse(text)
         assertElementText(function, text)
         XCTAssertEqual(function.name, "a")
         XCTAssertNil(function.returnType)
         XCTAssertFalse(function.throws)
     }
 
-    func test_parse_shouldParseFuncWithParameters() {
+    func test_parse_shouldParseFuncWithParameters() throws {
         let text = "func a(a: A, _ b: B)"
-        let function = parse(text)
+        let function = try parse(text)
         assertElementText(function, text)
         XCTAssertEqual(function.name, "a")
-        var param = function.parameters[0] as! ParameterImpl
-        assertElementText(param, "a: A", offset: 7)
+        var param = function.parameterClause.parameters[0]
+        assertElementText(param, "a: A")
         XCTAssertEqual(param.externalParameterName, nil)
         XCTAssertEqual(param.localParameterName, "a")
-        param = function.parameters[1] as! ParameterImpl
-        assertElementText(param, "_ b: B", offset: 13)
+        param = function.parameterClause.parameters[1]
+        assertElementText(param, "_ b: B")
         XCTAssertEqual(param.externalParameterName, "_")
         XCTAssertEqual(param.localParameterName, "b")
     }
 
-    func test_parse_shouldParseFuncWithReturnType() {
-        let text = "func a() -> Type"
-        let function = parse(text)
+    func test_parse_shouldParseFuncWithReturnType() throws {
+        let text = "func a() -> Int"
+        let function = try parse(text)
         assertElementText(function, text)
         XCTAssertEqual(function.name, "a")
-        let type = function.returnType
-        assertElementText(type, "Type", offset: 12)
+        let result = function.returnType
+        assertElementText(result, "-> Int")
+        assertElementText(result?.type, "Int")
     }
 
-    func test_parse_shouldParseThrowingFunc() {
+    func test_parse_shouldParseThrowingFunc() throws {
         let text = "func a() throws"
-        let function = parse(text)
+        let function = try parse(text)
         assertElementText(function, text)
         XCTAssertEqual(function.name, "a")
         XCTAssert(function.throws)
     }
 
-    func test_parse_shouldParseRethrowingFunc() {
+    func test_parse_shouldParseRethrowingFunc() throws {
         let text = "func a() rethrows"
-        let function = parse(text)
+        let function = try parse(text)
         assertElementText(function, text)
         XCTAssertEqual(function.name, "a")
         XCTAssertFalse(function.throws)
     }
 
-    func test_parse_shouldParseFuncWithDeclarationModifiers() {
+    func test_parse_shouldParseFuncWithDeclarationModifiers() throws {
         let text = "@a @objc(NS) public override mutating func a()"
-        let function = parse(text)
+        let function = try parse(text)
         assertElementText(function, text)
         XCTAssertEqual(function.name, "a")
     }
 
-    func test_parse_shouldParseFuncWithWhereClause() {
-        let text = "func a() where T: Type"
-        let function = parse(text)
+    func test_parse_shouldParseFuncWithWhereClause() throws {
+        let text = "func a() where T: U"
+        let function = try parse(text)
         assertElementText(function, text)
         XCTAssertEqual(function.name, "a")
     }
 
-    func test_parse_shouldParseFuncWithWhereClauseAndReturnStatement() {
-        let text = "func a() -> A where T: Type"
-        let function = parse(text)
+    func test_parse_shouldParseFuncWithWhereClauseAndReturnStatement() throws {
+        let text = "func a() -> A where T: U"
+        let function = try parse(text)
         assertElementText(function, text)
         XCTAssertEqual(function.name, "a")
     }
 
-    func test_parse_shouldParseFuncWithGenericParameterClause() {
+    func test_parse_shouldParseFuncWithGenericParameterClause() throws {
         let text = "func a<T, U: A & B>()"
-        let function = parse(text)
+        let function = try parse(text)
         assertElementText(function, text)
         XCTAssertEqual(function.name, "a")
-        assertElementText(function.genericParameterClause, "<T, U: A & B>", offset: 6)
+        assertElementText(function.genericParameterClause, "<T, U: A & B>")
     }
+
+    // TODO: test varargs
+    // TODO: test rethrows
 
     // MARK: - Helpers
 
-    func parse(_ text: String) -> FunctionDeclaration {
+    func parse(_ text: String) throws -> FunctionDeclaration {
         let parser = createDeclarationParser(text, .func, FunctionDeclarationParser.self)
-        return parser.parse()
+        return try parser.parse()
     }
 }
