300354da461f4605e168cf42ea0df0174aa5d285
Sean <seanhenry@nbnco.com.au>
2018-06-27 12:40:50 +1000

    Puncuation

--- SwiftStructureInterfaceTests/structure/parser/PunctuationParserTests.swift
+++ SwiftStructureInterfaceTests/structure/parser/PunctuationParserTests.swift
@@ -4,40 +4,40 @@ import XCTest
 class PunctuationParserTests: XCTestCase {
 
     func test_parsePunctuation() {
-        XCTAssertEqual(parse("->").text, "->")
-        XCTAssertEqual(parse(":").text, ":")
-        XCTAssertEqual(parse(",").text, ",")
-        XCTAssertEqual(parse(".").text, ".")
-        XCTAssertEqual(parse(";").text, ";")
-        XCTAssertEqual(parse("@").text, "@")
-        XCTAssertEqual(parse("#").text, "#")
-        XCTAssertEqual(parse("\\").text, "\\")
-        XCTAssertEqual(parse("(").text, "(")
-        XCTAssertEqual(parse(")").text, ")")
-        XCTAssertEqual(parse("{").text, "{")
-        XCTAssertEqual(parse("}").text, "}")
-        XCTAssertEqual(parse("[").text, "[")
-        XCTAssertEqual(parse("]").text, "]")
-        XCTAssertEqual(parse("<").text, "<")
-        XCTAssertEqual(parse("&").text, "&")
-        XCTAssertEqual(parse("_").text, "_")
+        XCTAssertEqual(try parse("->").text, "->")
+        XCTAssertEqual(try parse(":").text, ":")
+        XCTAssertEqual(try parse(",").text, ",")
+        XCTAssertEqual(try parse(".").text, ".")
+        XCTAssertEqual(try parse(";").text, ";")
+        XCTAssertEqual(try parse("@").text, "@")
+        XCTAssertEqual(try parse("#").text, "#")
+        XCTAssertEqual(try parse("\\").text, "\\")
+        XCTAssertEqual(try parse("(").text, "(")
+        XCTAssertEqual(try parse(")").text, ")")
+        XCTAssertEqual(try parse("{").text, "{")
+        XCTAssertEqual(try parse("}").text, "}")
+        XCTAssertEqual(try parse("[").text, "[")
+        XCTAssertEqual(try parse("]").text, "]")
+        XCTAssertEqual(try parse("<").text, "<")
+        XCTAssertEqual(try parse("&").text, "&")
+        XCTAssertEqual(try parse("_").text, "_")
     }
 
     func test_parseInvalid() {
-        XCTAssertEqual(parse("").text, "")
-        XCTAssertEqual(parse("+").text, "")
+        XCTAssertEqual(try parse("").text, "")
+        XCTAssertEqual(try parse("+").text, "")
     }
 
     func test_parseRightChevron() {
         let parser = createParser("<T>", PunctuationParser.self)
-        try! parser.parsePunctuation(.leftChevron)
-        parser.parseType()
+        _ = try! parser.parsePunctuation(.leftChevron)
+        _ = try! parser.parseType()
         XCTAssertEqual(try parser.parsePunctuation(.rightChevron).text, ">")
     }
 
     // MARK: - Helpers
 
-    func parse(_ input: String) -> LeafNode {
-        return createParser(input, PunctuationParser.self).parse()
+    func parse(_ input: String) throws -> LeafNode {
+        return try createParser(input, PunctuationParser.self).parse()
     }
 }
