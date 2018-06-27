97523cc8acda0998eb8abc97d9067a1982ece249
Sean <seanhenry@nbnco.com.au>
2018-06-27 12:29:59 +1000

    RequirementList

--- SwiftStructureInterface/structure/parser/RequirementListParser.swift
+++ SwiftStructureInterface/structure/parser/RequirementListParser.swift
@@ -1,42 +1,36 @@
 class RequirementListParser: Parser<RequirementList> {
 
-    override func parse(start: LineColumn) -> RequirementList {
-        var children = [Element?]()
-        children.append(parseRequirement())
-        while isNext(.comma) {
-            children.append(parseWhitespace())
-            children.append(try? parsePunctuation(.comma))
-            children.append(parseWhitespace())
-            children.append(parseRequirement())
+    override func parse() throws -> RequirementList {
+        return try RequirementListImpl(children: builder()
+                .required { try self.parseRequirement() }
+                .while(isParsed: { try self.parsePunctuation(.comma) }) {
+                    try self.parseRequirement()
+                }
+                .build())
+    }
+
+    private func parseRequirement() throws -> Requirement {
+        if let conformance = try? parseConformanceRequirement() {
+            return conformance
+        } else if let sameType = try? parseSameTypeRequirement() {
+            return sameType
         }
-        return RequirementListImpl(children: children)
+        throw LookAheadError()
     }
 
-    private func parseRequirement() -> Requirement {
-        let children: [Element] = [
-            parseType(),
-            parseWhitespace()
-        ]
-        return finishParsingRequirement(children)
+    private func parseConformanceRequirement() throws -> Requirement {
+        return try ConformanceRequirementImpl(children: builder()
+                .required { try self.parseType() }
+                .required { try self.parsePunctuation(.colon) }
+                .optional { try self.parseType() }
+                .build())
     }
 
-    private func finishParsingRequirement(_ children: [Element]) -> Requirement {
-        if isConformanceRequirement() {
-            return ConformanceRequirementImpl(children: children + [
-                try? parsePunctuation(.colon),
-                parseWhitespace(),
-                parseType()
-            ])
-        } else {
-            return SameTypeRequirementImpl(children: children + [
-                try? parseBinaryOperator("=="),
-                parseWhitespace(),
-                parseType()
-            ])
-        }
-    }
-
-    private func isConformanceRequirement() -> Bool {
-        return isNext(.colon)
+    private func parseSameTypeRequirement() throws -> Requirement {
+        return try SameTypeRequirementImpl(children: builder()
+                .required { try self.parseType() }
+                .required { try self.parseBinaryOperator("==") }
+                .optional { try self.parseType() }
+                .build())
     }
 }
--- SwiftStructureInterfaceTests/structure/parser/RequirementListParserTests.swift
+++ SwiftStructureInterfaceTests/structure/parser/RequirementListParserTests.swift
@@ -6,80 +6,90 @@ class RequirementListParserTests: XCTestCase {
     // MARK: - parse
 
     func test_parse_shouldParseEmptyString() {
-        assertText("", "")
+        XCTAssertThrowsError(try parse(""))
     }
 
-    func test_parse_shouldParseConformanceRequirement() {
+    func test_parse_shouldParseConformanceRequirement() throws {
         let text = "A:B"
-        let list = parse(text)
+        let list = try parse(text)
         XCTAssertEqual(list.text, text)
         let requirement = list.requirements[0] as! ConformanceRequirement
         XCTAssertEqual(requirement.leftTypeIdentifier.text, "A")
         XCTAssertEqual(requirement.rightTypeIdentifier.text, "B")
     }
 
-    func test_parse_shouldParseConformanceRequirementWithProtocolComposition() {
+    func test_parse_shouldParseConformanceRequirementWithProtocolComposition() throws {
         let text = "A:B&C"
-        let list = parse(text)
+        let list = try parse(text)
         XCTAssertEqual(list.text, text)
         let requirement = list.requirements[0] as! ConformanceRequirement
         XCTAssertEqual(requirement.leftTypeIdentifier.text, "A")
         XCTAssertEqual(requirement.rightProtocolCompositionType.text, "B&C")
     }
 
-    func test_parse_shouldParseSameTypeRequirement() {
+    func test_parse_shouldParseSameTypeRequirement() throws {
         let text = "A==B"
-        let list = parse(text)
+        let list = try parse(text)
         XCTAssertEqual(list.text, text)
         let requirement = list.requirements[0] as! SameTypeRequirement
         XCTAssertEqual(requirement.leftTypeIdentifier.text, "A")
         XCTAssertEqual(requirement.rightType.text, "B")
     }
 
-    func test_parse_shouldPartialParseWrongColon() {
-        assertText("A;B", "A")
+    func test_parse_shouldNotParseWrongColon() throws {
+        XCTAssertThrowsError(try parse("A;B"))
     }
 
-    func test_parse_shouldPartialParseMissingConformance() {
-        assertText("A", "A")
+    func test_parse_shouldNotParseMissingRequirement() throws {
+        XCTAssertThrowsError(try parse("A"))
     }
 
-    func test_parse_shouldParseMultipleConformances() {
-        assertText("A:B,C:D", "A:B,C:D")
+    func test_parse_shouldNotParseMultipleIncompleteConformances() throws {
+        XCTAssertThrowsError(try parse("A,B;C"))
     }
 
-    func test_parse_shouldTryToParseMultipleIncompleteConformances() {
-        assertText("A,B;C", "A,B")
+    func test_parse_shouldParseFirstPartOfInvalidClause() throws {
+        try assertText("A:B,C;D", "A:B,")
+        try assertText("A==B,C=D", "A==B,")
     }
 
-    func test_parse_shouldParseNestedTypes() {
-        assertText("A.B.C:D.E.F", "A.B.C:D.E.F")
-        assertText("A.B.C==D.E.F", "A.B.C==D.E.F")
+    func test_parse_shouldParseFirstPartOfInvalidRequirement() throws {
+        try assertText("A:", "A:")
+        try assertText("A==0", "A==")
     }
 
-    func test_parse_shouldParseProtocolCompositionInConformanceRequirement() {
-        assertText("A:B & C & D", "A:B & C & D")
+    func test_parse_shouldParseMultipleConformances() throws {
+        try assertText("A:B,C:D", "A:B,C:D")
     }
 
-    func test_parse_shouldParseProtocolCompositionInSameTypeRequirement() {
-        assertText("A==B & C & D", "A==B & C & D")
+    func test_parse_shouldParseNestedTypes() throws {
+        try assertText("A.B.C:D.E.F", "A.B.C:D.E.F")
+        try assertText("A.B.C==D.E.F", "A.B.C==D.E.F")
     }
 
-    func test_parse_shouldParseWhitespaceInSameTypeRequirement() {
-        assertText("A == B , C == D", "A == B , C == D")
+    func test_parse_shouldParseProtocolCompositionInConformanceRequirement() throws {
+        try assertText("A:B & C & D", "A:B & C & D")
     }
 
-    func test_parse_shouldParseWhitespaceInConformanceTypeRequirement() {
-        assertText("A : B , C : D & E", "A : B , C : D & E")
+    func test_parse_shouldParseProtocolCompositionInSameTypeRequirement() throws {
+        try assertText("A==B & C & D", "A==B & C & D")
+    }
+
+    func test_parse_shouldParseWhitespaceInSameTypeRequirement() throws {
+        try assertText("A == B , C == D", "A == B , C == D")
+    }
+
+    func test_parse_shouldParseWhitespaceInConformanceTypeRequirement() throws {
+        try assertText("A : B , C : D & E", "A : B , C : D & E")
     }
 
     // MARK: - Helpers
 
-    func assertText(_ input: String, _ expected: String, line: UInt = #line) {
-        XCTAssertEqual(parse(input).text, expected, line: line)
+    func assertText(_ input: String, _ expected: String, line: UInt = #line) throws {
+        XCTAssertEqual(try parse(input).text, expected, line: line)
     }
 
-    func parse(_ input: String) -> RequirementList {
-        return createParser(input, RequirementListParser.self).parse()
+    func parse(_ input: String) throws -> RequirementList {
+        return try createParser(input, RequirementListParser.self).parse()
     }
 }
