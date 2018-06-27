68650bca803aeb7411e1bdf17930d5744e059ed9
Sean <seanhenry@nbnco.com.au>
2018-06-27 14:36:06 +1000

    IdentifierParserTest

--- SwiftStructureInterfaceTests/structure/parser/IdentifierParserTests.swift
+++ SwiftStructureInterfaceTests/structure/parser/IdentifierParserTests.swift
@@ -4,24 +4,24 @@ import XCTest
 class IdentifierParserTests: XCTestCase {
 
     func test_shouldParseIdentifier() {
-        XCTAssertEqual(parse("identifier").text, "identifier")
+        XCTAssertEqual(try parse("identifier").text, "identifier")
     }
 
     func test_shouldAdvanceLexer() {
         let parser = createParser("identifier1 identifier2", IdentifierParser.self)
-        XCTAssertEqual(parser.parse().text, "identifier1")
-        XCTAssertEqual(parser.parse().text, "identifier2")
+        XCTAssertEqual(try parser.parse().text, "identifier1")
+        XCTAssertEqual(try parser.parse().text, "identifier2")
     }
 
     func test_shouldReturnEmptyIdentifier() {
-        XCTAssert(parse("()") === IdentifierImpl.emptyIdentifier)
+        XCTAssert(try parse("()") === IdentifierImpl.emptyIdentifier)
     }
 
     func test_shouldParseEscapedIdentifier() {
-        XCTAssertEqual(parse("`func`").text, "`func`")
+        XCTAssertEqual(try parse("`func`").text, "`func`")
     }
 
-    private func parse(_ input: String) -> Identifier {
-        return createParser(input, IdentifierParser.self).parse()
+    private func parse(_ input: String) throws -> Identifier {
+        return try createParser(input, IdentifierParser.self).parse()
     }
 }
