8af60d7424cd31a82e934c8ea6be77034cc1a8fd
Sean Henry <sean.henry6@gmail.com>
2020-04-13 16:56:11 +0100

    Adds validation to CLI

--- genmock/main.swift
+++ genmock/main.swift
@@ -3,13 +3,6 @@ import Foundation
 import SwiftyKit
 import MockGenerator
 
-func validateTargetNames(_ targetNames: [String]) throws -> [String] {
-    if targetNames.isEmpty {
-        throw error("You must specify at least one --target-name")
-    }
-    return targetNames
-}
-
 func validateTemplateName(_ templateName: String) throws -> String {
     guard validTemplateNames.contains(templateName) else {
         throw error("You must choose a valid template name: \(formattedValidTemplateNames)")
@@ -17,8 +10,8 @@ func validateTemplateName(_ templateName: String) throws -> String {
     return templateName
 }
 
-var formattedValidTemplateNames: String = validTemplateNames.joined(separator: "|")
 let validTemplateNames = ["spy", "stub", "dummy", "partial"]
+var formattedValidTemplateNames: String = validTemplateNames.joined(separator: "|")
 
 command(
     Argument<String>(
@@ -28,8 +21,7 @@ command(
     VariadicOption<String>(
         "target-name",
         default: [],
-        description: "The name of the protocol or class to mock. You may specify more that one e.g. --target-name MyProtocol --target-name MyClass",
-        validator: validateTargetNames
+        description: "The name of the protocol or class to mock. You may specify more that one e.g. --target-name MyProtocol --target-name MyClass"
     ),
     VariadicOption<String>(
         "import",
@@ -60,7 +52,8 @@ command(
     ),
     Option<String>(
         "mock-postfix",
-        default: "The string to append to the target name when creating the mock class."
+        default: "",
+        description: "The string to append to the target name when creating the mock class."
     ),
     Option<String>(
         "out-dir",
@@ -73,6 +66,13 @@ command(
         description: "The comment displayed at the top of the file to indicate that it is a generated file."
     )
 ) { directory, targetNames, imports, testableImports, useTabs, indentationWidth, templateName, prefix, postfix, outDir, generationComment in
+    if targetNames.isEmpty {
+        throw error("You must specify at least one --target-name")
+    }
+    if prefix.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+        && postfix.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+        throw error("You must specify --mock-prefix and/or --mock-postfix")
+    }
     setUpSwifty(
         projectURL: URL(fileURLWithPath: directory),
         useTabs: useTabs,
